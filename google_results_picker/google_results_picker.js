d1d98befe36e1a4225e780c616b21b3882aa82c6
Ion Paciu <ionpaciu999@gmail.com>
2021-04-26 12:06:12 +0200

    prettier

--- google_results_picker/google_results_picker.js
+++ google_results_picker/google_results_picker.js
@@ -5,22 +5,22 @@ var keys = {38: 1, 40: 1};
 var select_search_key = 76; // L
 
 function preventDefault(e) {
-  e.preventDefault();
+	e.preventDefault();
 }
 
 function preventDefaultForScrollKeys(e) {
-  if (keys[e.keyCode]) {
-    preventDefault(e);
-    return false;
-  }
+	if (keys[e.keyCode]) {
+		preventDefault(e);
+		return false;
+	}
 }
 
 // modern Chrome requires { passive: false } when adding event
 var supportsPassive = false;
 try {
-  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
-    get: function () { supportsPassive = true; } 
-  }));
+	window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+		get: function () { supportsPassive = true; } 
+	}));
 } catch(e) {}
 
 var wheelOpt = supportsPassive ? { passive: false } : false;
@@ -28,19 +28,19 @@ var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewh
 
 // call this to Disable
 function disableScroll() {
-  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+	window.addEventListener('keydown', preventDefaultForScrollKeys, false);
 }
 
 // call this to Enable
 function enableScroll() {
-  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+	window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
 }
 
 
 function selectResult(newId){
 	els = Array.from(document.querySelectorAll(resultElementSelector)).filter(function isValid(el) {
-			return el == null || !el.hasAttribute('decode-data-ved') && isValid(el.parentElement) // ignore results in "asked questions"
-		});
+		return el == null || !el.hasAttribute('decode-data-ved') && isValid(el.parentElement) // ignore results in "asked questions"
+	});
 	
 	if(newId >= els.length)
 		newId = els.length-1
@@ -75,16 +75,13 @@ document.onkeyup=function(event){
 		if(event.keyCode >= 49 && event.keyCode <= 57) {
 			// number keys
 			selectResult(event.keyCode-49)
-		}
-		if(event.keyCode==38) {
+		} else if(event.code == ArrowUp) {
 			// up
 			selectResult(document.selectedResultId-1)
-		}
-		if(event.keyCode==40) {
+		} else if (event.keyCode==40) {
 			// down
 			selectResult(document.selectedResultId+1)
-		}
-		if(event.keyCode==13) {
+		} else if (event.keyCode==13) {
 			// enter
 			rp = document.getElementById("result-pointer")
 			if(rp != null) {
@@ -97,19 +94,18 @@ document.onkeyup=function(event){
 					document.location = url
 				}
 			}
-		}
-		if(event.keyCode==select_search_key) {
+		} else if(event.keyCode==select_search_key) {
 			sb = document.querySelector('input[name="q"]');
 			sb.select();
 		}
-	} else if(event.keyCode==9) {
+	} else if (event.keyCode==9) {
 		document.selectedResultId=0
 		selectResult(0)
 		disableScroll()
 	}
 }
 if(document.selectedResultId==null) {
-document.selectedResultId=0
-selectResult(0)
-disableScroll()
+	document.selectedResultId=0
+	selectResult(0)
+	disableScroll()
 }
